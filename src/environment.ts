d4e60ae0e5c8fb3cdb1050de849d2bacad5a83e4
witt3rd <witt3rd@witt3rd.com>
2021-02-02 03:13:55 -0800

    update dependencies; default values for environment vars for better out-of-box experience

--- src/environment.ts
+++ src/environment.ts
@@ -15,11 +15,13 @@ interface Environment {
 
 export const environment: Environment = {
   apollo: {
-    introspection: process.env.APOLLO_INTROSPECTION === "true",
-    playground: process.env.APOLLO_PLAYGROUND === "true"
+    introspection: process.env.APOLLO_INTROSPECTION
+      ? process.env.APOLLO_INTROSPECTION === "true"
+      : true,
+    playground: process.env.APOLLO_PLAYGROUND ? process.env.APOLLO_PLAYGROUND === "true" : true,
   },
-  port: process.env.PORT!,
-  serviceId: process.env.SERVICE_ID!,
+  port: process.env.PORT || 8080,
+  serviceId: process.env.SERVICE_ID || "maana-my-service",
   outgoingConnectionTimeout: parseInt(process.env.OUTGOING_CONNECTION_TIMEOUT!),
-  maxQueryResults: parseInt(process.env.MAX_QUERY_RESULTS!)
+  maxQueryResults: parseInt(process.env.MAX_QUERY_RESULTS!),
 };
